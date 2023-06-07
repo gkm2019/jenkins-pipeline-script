20bcece7553cfc9dab141ff4874ae9e4f59cebd3
Kyeong Min <kkm1225@lguplus.co.kr>
2023-06-07 13:28:40 +0900

    DAXTEAM jenkins param test

--- jenkinsfiles/jenkins-stage-test.groovy
+++ jenkinsfiles/jenkins-stage-test.groovy
@@ -2,63 +2,6 @@ pipeline {
     agent any
     stages {
 
-        stage('Parameters'){
-            steps {
-                script {
-                    properties([
-                            parameters([
-                                    [$class: 'ChoiceParameter',
-                                     choiceType: 'SERVICE_JAR_FILE',
-                                     description: 'Select the jar file from the Dropdown List',
-                                     filterLength: 1,
-                                     filterable: false,
-                                     name: 'jar_file',
-                                     script: [
-                                             $class: 'GroovyScript',
-                                             fallbackScript: [
-                                                     classpath: [],
-                                                     sandbox: false,
-                                                     script:
-                                                             "return['Could not get The jar_file']"
-                                             ],
-                                             script: [
-                                                     classpath: [],
-                                                     sandbox: false,
-                                                     script:
-                                                             "return['1_jar_file','2_jar_file','3_far_file']"
-                                             ]
-                                     ]
-                                    ],
-                                    [$class: 'ChoiceParameter',
-                                     choiceType: 'BRANCH',
-                                     description: 'Select the branch from the Dropdown List',
-                                     filterLength: 1,
-                                     filterable: false,
-                                     name: 'jar_file',
-                                     script: [
-                                             $class: 'GroovyScript',
-                                             fallbackScript: [
-                                                     classpath: [],
-                                                     sandbox: false,
-                                                     script:
-                                                             "return['Could not get branch']"
-                                             ],
-                                             script: [
-                                                     classpath: [],
-                                                     sandbox: false,
-                                                     script:
-                                                             "return['main','dev']"
-                                             ]
-                                     ]
-                                    ],
-                                    [$class: 'StringParameterValue', name: 'service_name', value: "${JOB_NAME}"]
-
-                            ])
-                    ])
-                }
-            }
-        }
-
         stage('Stage 1') {
             steps {
                 script {
